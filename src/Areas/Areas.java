2086c70be5c22c6417b739df097b109d2f9e2713
Rubén Núñez Valle <rubennuvalle@gmail.com>
2018-12-11 14:01:33 +0100

    second commit

--- src/Areas/Areas.java
+++ src/Areas/Areas.java
@@ -5,9 +5,9 @@ public class Areas {
 		System.out.println("	CÁLCULO DE AREAS	\n==============================\n\n1.Calcular el área de un triángulo\n2. Calcular el área de un trapecio\n3.Calcular el área de un rectágunlo\n4.Salir");;
 		}
 		
-	public static double areaRectangulo(double base, double altura){
+	public static float areaRectangulo(float base, float altura){
 		boolean valid = true;
-		double resultado=0;
+		float resultado=0;
 		if (base < 2 || base > 10 ) {
 			System.out.println("Base no valida");
 			valid = false;
@@ -22,6 +22,29 @@ public class Areas {
 			
 		}else {
 			System.out.println("No se ha podido calcular el area de su rectángulo");
+		}
+		return resultado;
+	}
+	public static float areaTriangulo(float base , float altura) {
+		boolean valid = true;
+		float resultado=0;
+		if (base <=0 || altura <= 0 ) {
+			System.out.println("La base o la altura no son validas");
+			valid = false;
+		}
+		if (valid = true ) {
+			resultado=(base * altura) /2;
+		}
+		return resultado; 
+	}
+	public static float areaTrapecio(float base1, float base2, float altura ) {
+		float resultado=0;
+		if (base2 > 0 && base1 >0 && base1!=base2) {
+			double mediana = 0;
+			mediana = (base2 +base1)/2;	
+			resultado = (float) (altura * mediana) ;
+		}else {
+			System.out.println("La bases son negativas o bien ambas bases son de igual longitud");
 		}
 		return resultado;
 	}
--- src/Areas/TestAreas.java
+++ src/Areas/TestAreas.java
@@ -1,10 +1,56 @@
 package Areas;
-
+import java.util.Scanner;
 public class TestAreas {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Areas.menu();
+		Scanner sc = new Scanner (System.in);
+		for(int i=0;i<5;i++) {
+			i=0;
+			Areas.menu();
+			float resultado=0;
+			int opcion=0;
+			float base1=0;
+			float  altura=0;
+			float base2=0;
+			opcion = sc.nextInt();
+			switch (opcion) {
+				case 1:
+					System.out.println("Introduce el valor para la base del triángulo");
+					base1=sc.nextFloat();
+					System.out.println("Introduce el valor para la altura del triángulo");
+					altura = sc.nextFloat();
+					resultado=Areas.areaTriangulo(base1, altura);
+					if (resultado!=0)
+						System.out.printf("El area del triángulo es de %.2f%n",resultado);
+					break;
+				case 2:
+					System.out.println("Introduce el valor para la base 1 del trapecio");
+					base1=sc.nextFloat();
+					System.out.println("Introduce el valor para la base 2 del trapecio");
+					base2 = sc.nextFloat();
+					System.out.println("Introduce el valor para la altura del trapecio");
+					altura = sc.nextFloat();
+					resultado = Areas.areaTrapecio(base1, base2, altura);
+					if (resultado>0) {
+						System.out.printf("El area del trapecio es de %.2f%n",resultado);
+					}
+					break;
+				case 3:
+					System.out.println("Introduce el valor para la base del rectángulo");
+					base1=sc.nextFloat();
+					System.out.println("Introduce el valor para la altura del trectángulo");
+					altura = sc.nextFloat();
+					resultado=Areas.areaRectangulo(base1, altura);
+					if (resultado!=0)
+						System.out.printf("El area del triángulo es de %.2f%n",resultado);
+					break;
+				case 4: 
+					i=6;
+					sc.close();
+					break;
+			}
+		}
 	}
 
 }
